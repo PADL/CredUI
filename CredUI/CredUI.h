850e239536c6b531780201c006a9566fec36a3ed
Luke Howard <lukeh@padl.com>
2013-11-07 15:42:16 +1100

    Initial Commit

---
+++ CredUI/CredUI.h
@@ -0,0 +1,13 @@
+//
+//  CredUI.h
+//  CredUI
+//
+//  Created by Luke Howard on 7/11/2013.
+//  Copyright (c) 2013 PADL Software Pty Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CredUI : NSObject
+
+@end
---
+++ CredUI/CredUI.m
@@ -0,0 +1,13 @@
+//
+//  CredUI.m
+//  CredUI
+//
+//  Created by Luke Howard on 7/11/2013.
+//  Copyright (c) 2013 PADL Software Pty Ltd. All rights reserved.
+//
+
+#import "CredUI.h"
+
+@implementation CredUI
+
+@end
