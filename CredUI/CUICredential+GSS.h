919120e1eb473a7279d7a21fccc37fc1737c58e4
Luke Howard <lukeh@padl.com>
2014-01-06 15:03:48 +1100

    notes

--- CredUI/CUICredential+GSS.h
+++ CredUI/CUICredential+GSS.h
@@ -21,4 +21,15 @@
 struct GSSItem;
 
 - (struct GSSItem *)copyMatchingGSSItem:(BOOL)addIfAbsent error:(NSError * __autoreleasing *)error CF_RETURNS_RETAINED;
+
+/*
+ * Why is there no method to acquire a credential? gss_aapl_initial_cred() does not
+ * yet have enough logic to support arbitrary credential dictionaries (although you
+ * can acquire a password or certificate credential by passing it the result of calling
+ * [cred attributesWithClass:CUIAttributeClassGSSInitialCred).
+ *
+ * Instead, you need to use GSSKit, but at this stage I wanted to avoid creating a
+ * dependency on that from CredUI. There is a separate framework, GSSKitUI, on which
+ * you can use [GSSCredential initWithCUICredential:cred].
+ */
 @end
